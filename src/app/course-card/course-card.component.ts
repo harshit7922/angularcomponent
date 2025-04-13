8e84c14ba43733d1ec02d0aa63d773cb375d6019
harshit7922 <harshit.porwal78@gmail.com>
2025-04-14 03:51:20 +0530

    ngswitch case default

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { NgIf, NgClass } from '@angular/common';
+import { NgIf, NgClass, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { Course } from '../model/course';
 
 @Component({
@@ -7,7 +7,7 @@ import { Course } from '../model/course';
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.css',
   standalone: true, // Ensure this is a standalone component
-  imports: [NgIf,NgClass] // Add NgIf to imports
+  imports: [NgIf,NgClass,NgSwitch,  NgSwitchCase, NgSwitchDefault] // Add NgIf to imports
 })
 export class CourseCardComponent implements OnInit {
 @Input({required: true})
