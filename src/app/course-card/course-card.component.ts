29011db2185213b2088b8a951feaf41eb6ce5512
harshit7922 <harshit.porwal78@gmail.com>
2025-04-13 23:45:13 +0530

    created first custom component using command - ng generate component course-card

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css'
+})
+export class CourseCardComponent implements OnInit {
+
+  constructor() { }
+  ngOnInit(): void {
+  }
+}
